cdfb9d188df772f79da63218be63c2b9633970e4
Jeremiah dufourq <jezdufourq@gmail.com>
2020-08-24 20:38:25 +1000

    add: parallel dots and twitter API's.

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,18 +1,50 @@
 var express = require("express");
 var router = express.Router();
 
-//importing twitter service
-var twitterService = require("../services/twitter");
-var parallelDotsService = require("../services/parallelDots");
-const { $for } = require("core-js");
+//Twitter service
+var Twitter = require("twitter");
+var client = new Twitter({
+    consumer_key: process.env.TWITTER_CONSUMER_KEY,
+    consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+    bearer_token: process.env.TWITTER_BEARER_TOKEN
+});
+
+//Parallel dots
+const pd = require('paralleldots');
+pd.apiKey = process.env.PARALLEL_DOTS_API_KEY;
+
+
+
+var params = {
+    q: '$APT',
+    lang: 'en',
+    result_type: 'recent',
+    count: 50
+}
+
 
 /* GET METHODS */
 router.get("/search", function(req, res, next) {
-    const str =  'This is a test';
-
-    res.writeHead(200,{'content-type': 'text/html'});
-    res.write(str);
-    res.end();
+    client.get('search/tweets', params, gotData);
 });
+
+
+function gotData(err, data, res) {
+    var outputArr = []
+    console.log(data);
+    data.statuses.forEach((element) => {
+        outputArr.push(element.text);
+        console.log(element.text);
+    })
+    var parallelDotsArr = JSON.stringify(outputArr);
+
+    pd.sentiment(parallelDotsArr, 'en')
+        .then((res) => {
+            console.log(res);
+        })
+        .catch((err) => {
+            console.log(err);
+        })
+}
 
 module.exports = router;
--- server/services/twitter.js
+++
@@ -1,9 +0,0 @@
-var express = require("express");
-var Twitter = require("twitter");
-
-//token authentication using env variables
-var client = new Twitter({
-    consumer_key: process.env.TWITTER_CONSUMER_KEY,
-    consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
-    bearer_token: process.env.TWITTER_BEARER_TOKEN
-  });
