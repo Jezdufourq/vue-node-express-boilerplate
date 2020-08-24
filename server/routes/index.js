d1e91a952ca944685b02536e7436470c0d2a7259
Jeremiah dufourq <jezdufourq@gmail.com>
2020-08-24 21:41:53 +1000

    add: functions to handle twitter and parallel dots APIs.

--- server/routes/index.js
+++ server/routes/index.js
@@ -13,31 +13,39 @@ var client = new Twitter({
 const pd = require('paralleldots');
 pd.apiKey = process.env.PARALLEL_DOTS_API_KEY;
 
-
-
 var params = {
     q: '$APT',
     lang: 'en',
     result_type: 'recent',
     count: 50
 }
-
+var twitterOutputArr = []
 
 /* GET METHODS */
-router.get("/search", function(req, res, next) {
-    client.get('search/tweets', params, gotData);
+router.get("/search", async function(req, res, next) {
+    // TODO: handle the request (i.e put the query into the params object)
+    // TODO: need to also sanitise the query (there should be validation on the frontend as well).
+
+    //defining the params
+    var ticker = req.query.ticker;
+    // setting the params to the ticker from the GET method
+    params.q = ticker;
+
+    // send the data to the twitter API
+    var twitterData = await getTwitterData();
+
+    // get the sentiment
+    var parallelResp = await getParallelData(twitterData);
+
+    // sending response back
+    res.writeHead(200, {'Content-Type': 'application/json'});
+    res.end(JSON.stringify(parallelResp), 'utf-8');
 });
 
 
-function gotData(err, data, res) {
-    var outputArr = []
-    console.log(data);
-    data.statuses.forEach((element) => {
-        outputArr.push(element.text);
-        console.log(element.text);
-    })
-    var parallelDotsArr = JSON.stringify(outputArr);
-
+async function getParallelData(twitterData) {
+    var parallelDotsArr = JSON.stringify(twitterOutputArr);
+    console.log(parallelDotsArr);
     pd.sentiment(parallelDotsArr, 'en')
         .then((res) => {
             console.log(res);
@@ -45,6 +53,19 @@ function gotData(err, data, res) {
         .catch((err) => {
             console.log(err);
         })
+}
+async function getTwitterData(err, data, res) {
+    var outputArr = []
+    client.get('search/tweets', params)
+        .then((res) => {
+            res.statuses.forEach((element) => {
+                outputArr.push(element.text);
+            })
+        })
+        .catch((err) =>{
+            console.log(err);
+        });
+    return outputArr
 }
 
 module.exports = router;
