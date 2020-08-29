5d2e38a77548fd9d94c2cbe8fbcadbb640a5e681
Jeremiah dufourq <jezdufourq@gmail.com>
2020-08-29 13:08:01 +1000

    add: tweets-detailed to tweets.js.

--- server/routes/tweets.js
+++ server/routes/tweets.js
@@ -19,11 +19,26 @@ router.get("/tweets", async function(req, res, next) {
     // setting the params to the ticker from the GET method
     params.q = ticker;
     // send the data to the twitter API
-    var parallelResp = await twitterUtil.getTweetsDetailed(params).catch((error) => {console.log(error);});
+    var twitterResp = await twitterUtil.getTweetsText(params).catch((error) => {console.log(error);});
 
     // sending response back
     res.writeHead(200, {'Content-Type': 'application/json'});
-    res.end(JSON.stringify(parallelResp), 'utf-8');
+    res.end(JSON.stringify(twitterResp), 'utf-8');
 })
+
+router.get("/tweets-detailed", async function(req, res, next) {
+    //defining the params
+    var ticker = req.query.ticker;
+    // setting the params to the ticker from the GET method
+    params.q = ticker;
+    // send the data to the twitter API
+    var twitterResp = await twitterUtil.getTweetsDetailed(params).catch((error) => {console.log(error);});
+
+    // sending response back
+    res.writeHead(200, {'Content-Type': 'application/json'});
+    res.end(JSON.stringify(twitterResp), 'utf-8');
+})
+
+
 
 module.exports = router;
